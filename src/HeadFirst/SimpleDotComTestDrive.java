18c68b0c2b25f10b60d552b327036efd7c9b59eb
katorzhin <alex.katorzhin@gmail.com>
2019-02-13 13:13:55 +0200

    Initial commit

---
+++ src/HeadFirst/SimpleDotComTestDrive.java
@@ -0,0 +1,23 @@
+package HeadFirst;
+
+import HeadFirst.SimpleDotCom;
+
+public class SimpleDotComTestDrive {
+    public static void main(String[] args) {
+        SimpleDotCom dot = new SimpleDotCom();
+
+        int[] locations = {2, 3, 4};
+
+        dot.setLocationCells(locations);
+
+        String userGuess = "3";
+
+        String result = dot.checkYourself(userGuess);
+        String testResult = "Неудача";
+        if (result.equals("Попал")) {
+            testResult = "Пройден";
+        }
+        System.out.println(testResult);
+    }
+
+}
