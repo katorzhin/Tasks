b5c12b31dfabf50a87edd6060069149e4dfec31c
Alex Katorzhin <46688550+katorzhin@users.noreply.github.com>
2019-02-20 17:32:57 +0200

    Update IncrementalNumbersTask3.java

--- src/IncrementalNumbersTask3.java
+++ src/IncrementalNumbersTask3.java
@@ -3,13 +3,6 @@ package Tasks;
 public class IncrementalNumbersTask3 {
     public static void main(String[] args) {
         int[] array = {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        if (array[0] > 0) {
-            System.out.println("number positive ");
-        }
-        if (array[0] == 0) {
-            System.out.println("first number = 0 ");
-        } else {
-            System.out.println("number is negative");
-        }
+        System.out.println(array[0] > 0 ? "Положительное" : "Отрицательное");
     }
 }
