33023f6c0323e247f3eb3c7a24dd075954c4b6cf
katorzhin <alex.katorzhin@gmail.com>
2019-03-11 17:56:08 +0200

    completed area of triangle

--- src/Tasks/AreaOfTriangle.java
+++ src/Tasks/AreaOfTriangle.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class AreaOfTriangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите сторону А треугольника: ");
+        System.out.println("Enter the side A of the triangle:");
         double sideA = scanner.nextDouble();
 
-        System.out.println("Введите сторону B треугольника ");
+        System.out.println("Enter the side B of the triangle: ");
         double sideB = scanner.nextDouble();
 
-        System.out.println("Введите сторону C треугольника ");
+        System.out.println("Enter the side C of the triangle: ");
         double sideC = scanner.nextDouble();
 
         double perimeter2 = (sideA + sideB + sideC) / 2;
 
         double square = Math.sqrt(perimeter2 * (perimeter2 - sideA) * (perimeter2 - sideB) * (perimeter2 - sideC));
 
-        System.out.println("Площадь треугольника равна : "+ square);
+        System.out.println("The area of a triangle is: "+ square);
     }
 }
