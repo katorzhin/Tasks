e8a1e0d037cf90bc3b659b867166dcc684b2ec50
katorzhin <alex.katorzhin@gmail.com>
2019-04-03 11:21:46 +0300

    completed task4

--- src/ArrayList/Task4.java
+++ src/ArrayList/Task4.java
@@ -7,15 +7,21 @@ public class Task4 {
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(1);
         arrayList.add(2);
-        arrayList.add(3);
+        arrayList.add(9);
         arrayList.add(4);
         arrayList.add(6);
-        for (int i = 0; i < arrayList.size(); i++) {
+        boolean result = true;
+        for (int i = 0; i < arrayList.size() - 1; i++) {
             if (arrayList.get(i) < arrayList.get(i + 1)) {
-                System.out.println("increasing sequence");
             } else {
-                System.out.println("not increasing sequence");
+                result = false;
             }
+        }
+        if(result==true){
+            System.out.println("increasing");
+        }
+        else {
+            System.out.println("not");
         }
     }
 }
