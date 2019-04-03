ddffec04f04f8fa6bce1a86087101e65d1beb537
katorzhin <alex.katorzhin@gmail.com>
2019-04-03 11:31:58 +0300

    task5

---
+++ src/ArrayList/Task5.java
@@ -0,0 +1,23 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Task5 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(9);
+        arrayList.add(4);
+        arrayList.add(6);
+        ArrayList<Integer> newArrayList = new ArrayList<>();
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) % 2 == 0) {
+
+                newArrayList.add(i);
+
+            }
+        }
+        System.out.println(newArrayList);
+    }
+}
