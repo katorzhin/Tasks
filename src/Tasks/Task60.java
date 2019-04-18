67d1050ce18e17d543f63767c6115d2e048fd882
Alex Katorzhin <46688550+katorzhin@users.noreply.github.com>
2019-04-18 12:45:27 +0300

    Task60 is done

---
+++ src/Tasks/Task60.java
@@ -0,0 +1,20 @@
+package Tasks;
+
+import java.util.Arrays;
+
+public class Task60 {
+    public static void main(String[] args) {
+        int[] arrayA = {1, 2, 3, 4, 5};
+        int[] arrayB = {7, 88, 9, 99, 11};
+        int[] arrayC = new int[arrayA.length + arrayB.length];
+
+        for (int i = 0; i < arrayA.length; i++) {
+            arrayC[i] = arrayA[i];
+        }
+        for (int i = 0; i < arrayB.length; i++) {
+            arrayC[5 + i] = arrayB[i];
+        }
+
+        System.out.println(Arrays.toString(arrayC));
+    }
+}
