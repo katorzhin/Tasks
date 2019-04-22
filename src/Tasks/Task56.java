65a066542f00b33ba51c9f1eac62b462e83e67f3
katorzhin <alex.katorzhin@gmail.com>
2019-04-22 12:40:44 +0300

    Task56 is done

--- src/Tasks/Task56.java
+++ src/Tasks/Task56.java
@@ -1,24 +1,28 @@
 package Tasks;
 
-import java.util.Arrays;
-
 public class Task56 {
     public static void main(String[] args) {
-        int[] array = {1, 2, 2,4, 3, 3, 1};
-        int count = 0;
+        int[] array = {1,2,1};
         int[] newArray = new int[array.length];
+        int count = 0;
         for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == array[j]) {
-                    newArray[i] = array[j];
-                    count++;
-                    System.out.println(Arrays.toString(newArray));
-                }
-
+            if (!(contains(newArray,array[i]))){
+                newArray[i]=array[i];
+                count++;
             }
         }
+
         System.out.println(count);
 
+    }
+
+    public static boolean contains(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
