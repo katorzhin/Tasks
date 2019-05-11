3dc8bc1154adc788a022670077d5888c987ee82c
katorzhin <alex.katorzhin@gmail.com>
2019-05-11 23:47:22 +0300

    .

--- src/Tests/Interface/Application.java
+++ src/Tests/Interface/Application.java
@@ -2,7 +2,7 @@ package Tests.Interface;
 
 public class Application {
     public static void main(String[] args) {
-        KitchenStaff staff = new KitchenStaff();
-        staff.hiring();
+   //     KitchenStaff staff = new KitchenStaff();
+     //   staff.hiring();
     }
 }
