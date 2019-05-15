3c91177ba3cd5a5d65a36a9861076b0d6aa45edb
katorzhin <alex.katorzhin@gmail.com>
2019-05-15 18:09:09 +0300

    markdown parser

--- src/MarkDownParser.java
+++ src/MarkDownParser.java
@@ -1,18 +1,25 @@
 public class MarkDownParser {
 
-    static void Emphasi(String string) {
-        string = string.substring(1, string.length() - 1);
-        System.out.println("<em>" + string + "</em>");
-    }
-
-    static void SimpleString(String simpleString) {
-        System.out.println("<p>" + simpleString + "</p>");
-    }
-
     public static void main(String[] args) {
+        String newWord= "*dsad* sdf sd f ds";
+        //newWord = newWord.trim(); // This would remove trailing and leading spaces
+        String [] words = newWord.split("\\s+"); //split them on spaces
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<words.length;i++){
+            if (newWord.charAt(i)=='*') {
+                newWord = newWord.replaceAll("[*]", "");
+            }
+                sb.append("<em>");
+                sb.append(words[i]);
+                sb.append(' ');
 
-        Emphasi("*gdfgdf*");
-        SimpleString("skdfjs");
+        }
+      //  if(words.length>0){
+        //    sb.append('$');
+          //  sb.append(words[words.length-1]);
+        //}
+        newWord = sb.toString();
+        System.out.println(newWord);
     }
 }
 
