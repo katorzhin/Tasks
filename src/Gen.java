5b9807f3aedb12f92dd88e26c56f6dd75272f1b7
katorzhin <alex.katorzhin@gmail.com>
2019-06-13 18:35:06 +0300

    gen

---
+++ src/Gen.java
@@ -0,0 +1,19 @@
+public class Gen<T> {
+    T ob;
+
+    Gen(T o) {
+        ob = o;
+    }
+
+    T getOb() {
+        return ob;
+    }
+    void showType(){
+        System.out.println("Тип Т является "+ob.getClass().getName());
+    }
+}
+class GenDemo{
+    public static void main(String[] args) {
+        
+    }
+}
