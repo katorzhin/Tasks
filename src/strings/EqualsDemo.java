d038dbf7ddc56ca7639e498b8e53b99f47785de4
katorzhin <alex.katorzhin@gmail.com>
2019-06-19 23:36:35 +0300

    cafe

--- src/strings/EqualsDemo.java
+++ src/strings/EqualsDemo.java
@@ -3,13 +3,10 @@ package strings;
 public class EqualsDemo {
     public static void main(String[] args) {
         String s1 = "Hello";
-        String s2 = "Hello";
-        String s3 = "Good bye";
-        String s4 = "HELLO";
-        System.out.println(s1+" equally "+ s2+" -> "+s1.equals(s2));
-        System.out.println(s1+" equally "+ s3+" -> "+s1.equals(s3));
-        System.out.println(s1+" equally "+ s4+" -> "+s1.equals(s4));
-        System.out.println(s1+" equals case insensitive "+ s4+" -> "+s1.equalsIgnoreCase(s4));
+        String s2 = new String(s1);
+        System.out.println(s1+ " equally "+s2 + " -> "+s1.equals(s2));
+        System.out.println(s1+" == "+s2+" -> "+(s1==s2));
+
 
     }
 }
