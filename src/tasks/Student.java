c9130254f1d8964c5eac663c7254f4d5b23f04f6
katorzhin <alex.katorzhin@gmail.com>
2019-09-12 13:37:04 +0300

    module 6 tasks one and two

--- src/tasks/Student.java
+++ src/tasks/Student.java
@@ -1,8 +1,6 @@
 package tasks;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Student {
     private String name;
@@ -16,7 +14,6 @@ public class Student {
         return course;
     }
 
-
     private Student(String name, int course) {
         this.name = name;
         this.course = course;
@@ -28,28 +25,88 @@ public class Student {
         Iterator<Student> iterator = students.iterator();
         while (iterator.hasNext()) {
             Student student = iterator.next();
-            if (student.getCourse() ==course){
+            if (student.getCourse() == course) {
                 System.out.println(student.getName());
             }
         }
     }
 
+    static Set<Student> union(Set<Student> set1, Set<Student> set2) {
+        Set<Student> result = new HashSet<>();
+        result.addAll(set1);
+        result.addAll(set2);
+        return result;
+    }
+
+    static Set<Student> intersect(Set<Student> set1, Set<Student> set2) {
+        Set<Student> result = new HashSet<>();
+        for (Student a : set1) {
+            for (Student b : set2) {
+                if (set1.equals(set2)) {
+                    result.add(a);
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
     public static void main(String[] args) {
-        List<Student> list = new ArrayList<Student>();
+//        List<Student> list = new ArrayList<Student>();
+//        list.add(new Student("Julia", 5));
+//        list.add(new Student("Marina", 3));
+//        list.add(new Student("Katia", 1));
+//        list.add(new Student("Sergey", 4));
+//        int course = 5;
+//         printStudents(list,course);
+        Set<Student> firstSet = new HashSet<>();
+        firstSet.add(new Student("Marina", 7));
+        firstSet.add(new Student("Julias", 6));
+        Set<Student> secondSet = new HashSet<>();
+        secondSet.add(new Student("Marina", 7));
+        secondSet.add(new Student("Misha", 8));
+        //System.out.println(union(firstSet,secondSet));
+        System.out.println(intersect(firstSet, secondSet));
 
-        list.add(new Student("Vasia", 2));
-        list.add(new Student("Petia", 1));
-        list.add(new Student("Sasha", 4));
-        list.add(new Student("Masha", 3));
-        list.add(new Student("Dasha", 5));
-        list.add(new Student("Svetlana", 3));
-        list.add(new Student("Genadiy", 2));
-        list.add(new Student("Julia", 5));
-        list.add(new Student("Marina", 3));
-        list.add(new Student("Katia", 1));
-        list.add(new Student("Sergey", 4));
-
-        int course = 5;
-        printStudents(list,course);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
